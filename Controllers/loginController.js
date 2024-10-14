051420b47ae92922859edf90623a0da5a7335d47
ssadown <bibatututu@yandex.ru>
2024-10-14 21:50:29 +0500

    написал документацию

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -3,12 +3,13 @@ const loginData = require('../Data/loginData')
 class loginController {
     async getAccount(req,res) {
         try {
-            const { email } = req.body
-            console.log(email)
-            console.log(loginData)
+            const { email, password } = req.body
             const account = loginData.find(acc => acc.email === email)
+            if (account.password === password) {
+                res.status(200).json({account: account})
+            }
+                res.status(401).json({error: "Unauthorized"})
             console.log(account)
-            res.status(200).json({account: account})
         } catch (e) {
             res.status(500).json({error: "Account not found"})
         }
