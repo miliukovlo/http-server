64248117526ce5759e7b925aedf9e9f682627ae1
ssadown <bibatututu@yandex.ru>
2024-11-18 20:17:15 +0500

    Сделал получение определенной пары

--- Controllers/CoupleController.js
+++ Controllers/CoupleController.js
@@ -3,7 +3,16 @@ const coupleData = require('../Data/CoupleData')
 class CoupleController {
     async getCouple(req,res) {
         try {
-                res.status(200).json(coupleData)
+            res.status(200).json(coupleData)
+        } catch (e) {
+            res.status(500).json({error: "Server doesn`t work"})
+        }
+    }
+    async getCurrentCouple(req,res) {
+        try {
+            const {id} = req.body
+            const currentCouple = coupleData.find((lesson) => lesson.id === Number(id))
+            res.status(200).json(currentCouple)
         } catch (e) {
             res.status(500).json({error: "Server doesn`t work"})
         }
--- Router/CoupleRoutes.js
+++ Router/CoupleRoutes.js
@@ -3,5 +3,6 @@ const router = new Router()
 const couple = require('../Controllers/CoupleController')
 
 router.get('/couple', couple.getCouple)
+router.post('/couple', couple.getCurrentCouple)
 
 module.exports = router
