130c861dd52f8874582e866a818737030f98920a
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-08 11:48:26 +0500

    Added new route for proper client pagination

--- Controllers/generatedStudentsController.js
+++ Controllers/generatedStudentsController.js
@@ -1,36 +1,49 @@
-const generatedStudents = require('../Data/generatedStudents')
+const generatedStudents = require("../Data/generatedStudents");
 
 class allStudents {
-    async getAllStudents(req,res) {
-        try {
-            const students = generatedStudents
-            res.status(200).json(students)
-        } catch (e) {
-            res.status(500).json({error: 'Fail get students for users by server'})
-        }
-    }
-    async getLimitStudents(req,res) {
-        try {
-            const {limit} = req.params
-            if (generatedStudents.length < limit) {
-                const students = generatedStudents
-                res.status(200).json(students)
-            }
-            const students = generatedStudents.slice(0,limit)
-            res.status(200).json(students)
-        } catch (e) {
-            res.status(500).json({error: 'Fail get students for users by server'})
-        }
-    }
-    async getCurrentStudent(req,res) {
-        try {
-            const {id} = req.params
-            const student = generatedStudents.find(stud => stud._id === id)
-            res.status(200).json({student: student})
-        } catch (e) {
-            res.status(500).json({error: 'Fail get student for users/:id by server'})
-        }
-    }
+	async getAllStudents(req, res) {
+		try {
+			const students = generatedStudents;
+			res.status(200).json(students);
+		} catch (e) {
+			res.status(500).json({ error: "Fail get students for users by server" });
+		}
+	}
+
+	async getStudentsByPage(req, res) {
+		const { page, limit } = req.params;
+
+		const endIndex = page * limit;
+		const startIndex = (page - 1) * limit;
+
+		const students = generatedStudents.slice(startIndex, endIndex);
+		res.status(200).json(students);
+	}
+
+	async getLimitStudents(req, res) {
+		try {
+			const { limit } = req.params;
+			if (generatedStudents.length < limit) {
+				const students = generatedStudents;
+				res.status(200).json(students);
+			}
+			const students = generatedStudents.slice(0, limit);
+			res.status(200).json(students);
+		} catch (e) {
+			res.status(500).json({ error: "Fail get students for users by server" });
+		}
+	}
+	async getCurrentStudent(req, res) {
+		try {
+			const { id } = req.params;
+			const student = generatedStudents.find(stud => stud._id === id);
+			res.status(200).json({ student: student });
+		} catch (e) {
+			res
+				.status(500)
+				.json({ error: "Fail get student for users/:id by server" });
+		}
+	}
 }
 
-module.exports = new allStudents()
+module.exports = new allStudents();
--- Router/generateRoutes.js
+++ Router/generateRoutes.js
@@ -1,9 +1,13 @@
-const Router = require('express')
-const router = new Router()
-const allStudents = require(`../Controllers/generatedStudentsController`)
+const Router = require("express");
+const router = new Router();
+const allStudents = require(`../Controllers/generatedStudentsController`);
 
-router.get('/generate', allStudents.getAllStudents)
-router.get('/generate/limit/:limit', allStudents.getLimitStudents)
-router.get('/generate/current/:id', allStudents.getCurrentStudent)
+router.get("/generate", allStudents.getAllStudents);
+router.get("/generate/limit/:limit", allStudents.getLimitStudents);
+router.get("/generate/current/:id", allStudents.getCurrentStudent);
+router.get(
+	"/generate/students/page=:page/limit=:limit",
+	allStudents.getStudentsByPage
+);
 
-module.exports = router
+module.exports = router;
